0cb951d9235591e46cce1be0f0efe06ec2dd96ae
handsofblue67 <michael.robison168@gmail.com>
2015-05-26 14:37:28 -0600

    final...y

--- main.cpp
+++ main.cpp
@@ -8,14 +8,17 @@ using namespace std;
 int main()
 {
 	string answer = "";
-	string file = "";
+	string command;
+	ifstream in;
+	int intValue;
 
 	do
 	{
-		string command = "";
-		ifstream in;
+		
+		string file = "";
 		cout << "Enter the name of the file" << endl;
 		cin >> file;
+		
 
 		in.open(file);
 		if (in.fail())
@@ -26,46 +29,45 @@ int main()
 
 		else
 		{
-			
+	
+			while (!in.eof())
+			{
+				
 			Queue<int> queue;
 			Stack<int> stack;
-			int intValue;
-			string command;
-			int num;
-			
-			while (in >> command >> intValue)
-			{
 				{
-					if ( command == "push")
+					in >> command;
+					if (command == "push")
 					{
 						in >> intValue;
-						cout << "push\t\tstack\t\t" << intValue << "t\t" << stack.push(intValue) << endl;
+						cout << "push\t\tstack\t\t" << intValue << "\t\t" << stack.push(intValue) << endl;
 					}
 
 					else if (command == "pop")
 					{
-						num = stack.pop();
 						cout << "pop\t\tstack\t\t---\t\t" << stack.pop() << endl;
 					}
 
 					else if (command == "append")
 					{
-						
+
 						in >> intValue;
 						cout << "append\t\tqueue\t\t" << intValue << "\t\t" << queue.append(intValue) << endl;
 					}
-	
+
 					else if (command == "serve")
 					{
-						
+
 						cout << "serve\t\tqueue\t\t---\t\t" << queue.serve() << endl;
 					}
-				}	
+				}
 			}
-		}
-		in.close();
-		cout << "Enter 'y' if you want to enter another file" << endl;
-	}while (answer == "y");
-		system("pause");
-		return 0;
+
+			in.close();
+			cout << "Enter 'y' if you want to enter another file" << endl;
+			cin >> answer;
+		}	
+	} while (answer == "y");
+	system("PAUSE");
+	return 0;
 }
